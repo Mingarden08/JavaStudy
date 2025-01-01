c1a531b9d72f53021a2ae54892c1816029882f61
Mingarden08 <jeongone.min@gmail.com>
2025-01-01 19:50:50 +0900

    create Repository

---
+++ src/Java/SumOfDivisors.java
@@ -0,0 +1,14 @@
+package Java;
+
+public class SumOfDivisors {
+    public static void main(String[] args) {
+        int answer = 0;
+        int n = 12;
+        for(int i = 1; i<n+1; i++) {
+            if(n%i == 0) {
+                answer += i;
+            }
+        }
+        System.out.println(answer);
+    }
+}
