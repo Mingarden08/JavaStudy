af24259df79b372e426f37664aba0cfb650e5cd5
Mingarden08 <jeongone.min@gmail.com>
2025-01-02 10:19:43 +0900

    숫자 문자열과 영단어

---
+++ src/Java/NumericStringsAndEnglish_0102.java
@@ -0,0 +1,51 @@
+package Java;
+
+public class NumericStringsAndEnglish_0102 {
+    public static void main(String[] args) {
+        String s = "2three45sixseven";
+        String answer = "";
+        for(int i = 0; i<s.length(); i++) {
+            char tempChar = s.charAt(i);
+            switch (tempChar) {
+                case 'z' -> {
+                    i += 3; answer += "0";
+                }
+                case 'o' -> {
+                    i += 2; answer += "1";
+                }
+                case 't' -> {
+                    if (s.charAt(i+1) == 'w') {
+                        i += 2; answer += "2";
+                    }
+                    else {
+                        i += 4; answer += "3";
+                    }
+                }
+                case 'f' -> {
+                    if (s.charAt(i+1) == 'o') {
+                        i += 3; answer += "4";
+                    }
+                    else {
+                        i += 3; answer += "5";
+                    }
+                }
+                case 's' -> {
+                    if (s.charAt(i+1) == 'i') {
+                        i += 2; answer += "6";
+                    }
+                    else {
+                        i += 4; answer += "7";
+                    }
+                }
+                case 'e' -> {
+                    i += 4; answer += "8";
+                }
+                case 'n' -> {
+                    i += 3; answer += "9";
+                }
+                default -> answer += tempChar;
+            };
+        }
+        System.out.println(Integer.parseInt(answer));
+    }
+}
