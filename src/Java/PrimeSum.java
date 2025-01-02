0a03572b48538e7c83eacaf363a508e384d86da1
Mingarden08 <jeongone.min@gmail.com>
2025-01-02 11:35:07 +0900

    소수 찾기

---
+++ src/Java/PrimeSum.java
@@ -0,0 +1,21 @@
+package Java;
+
+public class PrimeSum {
+    public static void main(String[] args) {
+        int n = 10; int answer = 0;
+        for(int i = 2; i<=n; i++)
+            if (isPrime(i))
+                answer++;
+        System.out.println(answer);
+    }
+    public static boolean isPrime(int i) {
+        boolean flag = true;
+        for(int j = 2; j<=Math.sqrt(i); j++) {
+            if (i % j == 0) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+}
