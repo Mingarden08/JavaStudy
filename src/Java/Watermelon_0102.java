f45813531541cfd136afba4f72628d1161eaea6c
Mingarden08 <jeongone.min@gmail.com>
2025-01-02 09:52:52 +0900

    수박수박수박수박수박수?

---
+++ src/Java/Watermelon_0102.java
@@ -0,0 +1,14 @@
+package Java;
+
+public class Watermelon_0102 {
+    public static void main(String[] args) {
+        int n = 11; String answer = "";
+        for (int i = 0; i<n; i++) {
+            if (i % 2 == 0)
+                answer += "수";
+            if (i % 2 == 1)
+                answer += "박";
+        }
+        System.out.println(answer);
+    }
+}
