2745f55e570ab947b8adcf3c89cb7f2b20f482ce
Mingarden08 <jeongone.min@gmail.com>
2025-01-04 13:19:09 +0900

    배열 자르기

---
+++ src/Level_0/CutArray.java
@@ -0,0 +1,17 @@
+package Level_0;
+
+import java.util.Arrays;
+
+public class CutArray {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5}; int num1 = 2; int num2 = 3;
+        int[] answer = new int[num2 - num1+1];
+        for(int i = 0; i < answer.length; i++) {
+//            System.out.println(numbers.length);
+//            System.out.println(Arrays.toString(answer));
+//            System.out.println(num1+i);
+            answer[i] = numbers[num1+i];
+        }
+        System.out.println(Arrays.toString(answer));
+    }
+}
