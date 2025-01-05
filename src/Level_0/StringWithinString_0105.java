542ede8c56dcaadeee835152f419e0a6db8a68c4
Mingarden08 <jeongone.min@gmail.com>
2025-01-05 22:06:10 +0900

    오늘은 여기까지!!

---
+++ src/Level_0/StringWithinString_0105.java
@@ -0,0 +1,11 @@
+package Level_0;
+
+public class StringWithinString_0105 {
+    public static void main(String[] args) {
+        String str1 = "ab6CDE443fgh22iJKlmn1o"; String str2 = "6CD";
+        int answer = 0;
+        for(int i = 0; i<str1.length(); i++) {
+
+        }
+    }
+}
--- src/Level_1/IntegerDescendingOrder_0105.java
+++ src/Level_1/IntegerDescendingOrder_0105.java
@@ -1,14 +1,14 @@
-package Level_1;
-public class IntegerDescendingOrder_0105 {
-    public static void main(String[] args) {
-        long n = 118372;
-        long answer = 0;
-        String ansStr = "";
-        while (num > 0) {
-
-        }
-        sum += num%10;
-
-        num /= 10;
-    }
-}
+//package Level_1;
+//public class IntegerDescendingOrder_0105 {
+//    public static void main(String[] args) {
+//        long n = 118372;
+//        long answer = 0;
+//        String ansStr = "";
+//        while (num > 0) {
+//
+//        }
+//        sum += num%10;
+//
+//        num /= 10;
+//    }
+//}
