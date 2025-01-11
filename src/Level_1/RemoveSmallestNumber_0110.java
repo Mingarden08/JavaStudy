05d3054deea3207517a0527b911f09881a4db164
Mingarden08 <jeongone.min@gmail.com>
2025-01-11 22:15:53 +0900

    제일 작은 수 제거하기

--- src/Level_1/RemoveSmallestNumber_0110.java
+++ src/Level_1/RemoveSmallestNumber_0110.java
@@ -4,28 +4,27 @@ import java.util.*;
 
 public class RemoveSmallestNumber_0110 {
     public static void main(String[] args){
-        int[] arr = {2, 3, 1};
-        int min = arr[0];
-        if (arr.length <= 1) {
-            int[] answer = new int[arr.length];
-            answer[0] = -1;
-            System.out.println(answer[0]);
+        int[] arr = {4,3,2,1};
+        if (arr.length == 1) {
+            int[] answer = {-1};
+            System.out.println(answer);
         }
         int[] answer = new int[arr.length-1];
+        List<Integer> temp = new ArrayList<>();
+        int min = arr[0];
         for (int i = 0; i<arr.length; i++) {
-            if (min > arr[i]) {
+            if(min >= arr[i])
                 min = arr[i];
-            }
         }
-        for(int i = 0; i<arr.length; i++) {
-            if (min == arr[i]) {
+        for(int i = 0; i<arr.length; i++){
+            if (min == arr[i])
                 continue;
-            }
-            else {
-                answer[i] = arr[i];
-            }
+            else
+                temp.add(arr[i]);
         }
-        System.out.println(min);
-        System.out.println(Arrays.toString(answer));
+        for(int i = 0; i<temp.size(); i++) {
+            answer[i] = temp.get(i);
+        }
+        System.out.println(answer);
     }
 }
