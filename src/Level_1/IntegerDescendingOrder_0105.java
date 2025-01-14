2aac929e32a3a47ccf09814f0b641fc6b54de5eb
Mingarden08 <jeongone.min@gmail.com>
2025-01-14 20:25:18 +0900

    정수 내림차순으로 배치하기

--- src/Level_1/IntegerDescendingOrder_0105.java
+++ src/Level_1/IntegerDescendingOrder_0105.java
@@ -1,14 +1,22 @@
-//package Level_1;
-//public class IntegerDescendingOrder_0105 {
-//    public static void main(String[] args) {
-//        long n = 118372;
-//        long answer = 0;
-//        String ansStr = "";
-//        while (num > 0) {
-//
-//        }
-//        sum += num%10;
-//
-//        num /= 10;
-//    }
-//}
+package Level_1;
+
+import java.util.*;
+
+public class IntegerDescendingOrder_0105 {
+    public static void main(String[] args) {
+        long n = 118372;
+        long answer = 0;
+        List<Integer> tempArr = new ArrayList<>();
+        String tempAnswer = n + "";
+        for(int i = (int)Math.log10(n); i>=0; i--) {
+            tempArr.add((int)tempAnswer.charAt(i) - 48);
+        }
+        tempArr.sort(Comparator.reverseOrder());
+        tempAnswer = "";
+        for(int i = 0; i<tempArr.size(); i++) {
+            tempAnswer += tempArr.get(i);
+        }
+        answer = Long.parseLong(tempAnswer);
+        System.out.println(answer);
+    }
+}
