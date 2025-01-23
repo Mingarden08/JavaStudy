b6f5e1a253003fde3c42f68126744fa192b7a8fe
Mingarden08 <jeongone.min@gmail.com>
2025-01-23 20:55:22 +0900

    n의 배수

---
+++ src/Level_0/MultipleofN_0123.java
@@ -0,0 +1,12 @@
+package Level_0;
+
+public class MultipleofN_0123 {
+    public static void main(String[] args) {
+        int num = 34; int n = 3;
+        int answer = 0;
+        if (num % n == 0) {
+            answer = 1;
+        }
+        System.out.println(answer);
+    }
+}
