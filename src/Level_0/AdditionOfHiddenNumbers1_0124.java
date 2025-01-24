16b2d195ca1e8c37838b3b092be91b5e170c1d7e
Mingarden08 <jeongone.min@gmail.com>
2025-01-24 20:57:28 +0900

    숨어있는 숫자의 덧셈 (1)

---
+++ src/Level_0/AdditionOfHiddenNumbers1_0124.java
@@ -0,0 +1,13 @@
+package Level_0;
+
+public class AdditionOfHiddenNumbers1_0124 {
+    public static void main(String[] args) {
+        String my_string = "aAb1B2cC34oOp";
+        int answer = 0;
+        for(int i = 0; i<my_string.length(); i++) {
+            if (((int)my_string.charAt(i)) - 48 >= 1 && ((int)my_string.charAt(i)) - 48 <= 9)
+                answer += ((int)my_string.charAt(i)) - 48;
+        }
+        System.out.println(answer);
+    }
+}
