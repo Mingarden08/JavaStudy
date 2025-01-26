62fdc27fe613bbdb6f9620fd42c79c266a0e565a
Mingarden08 <jeongone.min@gmail.com>
2025-01-26 11:06:29 +0900

    문자열로 변환

---
+++ src/Level_0/ConvertToString_0126.java
@@ -0,0 +1,9 @@
+package Level_0;
+
+public class ConvertToString_0126 {
+    public static void main(String[] args) {
+        int n = 123;
+        String answer = String.valueOf(n);
+        System.out.println(answer);
+    }
+}
