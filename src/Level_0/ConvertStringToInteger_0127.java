657fa2513e6b71c7db0ed4da15c562a5207df5e7
Mingarden08 <jeongone.min@gmail.com>
2025-01-27 11:23:04 +0900

    문자열을 정수로 변환하기

---
+++ src/Level_0/ConvertStringToInteger_0127.java
@@ -0,0 +1,9 @@
+package Level_0;
+
+public class ConvertStringToInteger_0127 {
+    public static void main(String[] args) {
+        String n_str = "10";
+        int answer = Integer.valueOf(n_str);
+        System.out.println(answer);
+    }
+}
