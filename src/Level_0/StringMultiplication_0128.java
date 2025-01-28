94a6e8e9b7050fa10f51507366db6e587a291566
Mingarden08 <jeongone.min@gmail.com>
2025-01-28 21:43:48 +0900

    문자열 곱하기

---
+++ src/Level_0/StringMultiplication_0128.java
@@ -0,0 +1,13 @@
+package Level_0;
+
+public class StringMultiplication_0128 {
+    public static void main(String[] args) {
+        String my_string = "string";
+        int k = 3;
+        String answer = "";
+        for (int i = 0; i<k; i++) {
+            answer += my_string;
+        }
+        System.out.println(answer);
+    }
+}
