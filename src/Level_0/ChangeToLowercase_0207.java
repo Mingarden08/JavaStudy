ae6133dd32a59fddd999c8749be2c7bfc3abe4e0
Mingarden08 <jeongone.min@gmail.com>
2025-02-07 23:43:02 +0900

    소문자로 바꾸기

---
+++ src/Level_0/ChangeToLowercase_0207.java
@@ -0,0 +1,10 @@
+package Level_0;
+
+public class ChangeToLowercase_0207 {
+    public static void main(String[] args) {
+        String myString = "aBcDeF";
+        String answer = myString.toLowerCase();
+        System.out.println(answer);
+
+    }
+}
