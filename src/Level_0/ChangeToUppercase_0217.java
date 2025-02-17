a728f51b94d26c81a35d8aebdf287d9ea117b5b3
Mingarden08 <jeongone.min@gmail.com>
2025-02-17 23:40:40 +0900

    대문자로 바꾸기

---
+++ src/Level_0/ChangeToUppercase_0217.java
@@ -0,0 +1,9 @@
+package Level_0;
+
+public class ChangeToUppercase_0217 {
+    public static void main(String[] args) {
+        String myString = "abcDEfg";
+        String answer = myString.toUpperCase();
+        System.out.println(answer);
+    }
+}
