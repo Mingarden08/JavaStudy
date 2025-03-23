f0fe943950024dc98cb1b96ac934c9cedc4978ad
Mingarden08 <jeongone.min@gmail.com>
2025-03-23 21:45:42 +0900

    숫자 찾기

---
+++ src/Level_0/FindNumbers_0323.java
@@ -0,0 +1,17 @@
+package Level_0;
+
+public class FindNumbers_0323 {
+    public static void main(String[] args) {
+        int num = 232443;
+        int k = 4;
+        int answer = -1;
+        String temp = String.valueOf(num);
+        for (int i = 0; i<temp.length(); i++) {
+            if (temp.charAt(i) == k + '0') {
+                answer = i + 1;
+                break;
+            }
+        }
+        System.out.println(answer);
+    }
+}
