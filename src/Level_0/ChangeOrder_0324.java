24e3dff604bfafc00ef4316050bc0f77d730f9f7
Mingarden08 <jeongone.min@gmail.com>
2025-03-24 22:02:37 +0900

    순서 바꾸기

---
+++ src/Level_0/ChangeOrder_0324.java
@@ -0,0 +1,19 @@
+package Level_0;
+
+import java.util.Arrays;
+
+public class ChangeOrder_0324 {
+    public static void main(String[] args) {
+        int[] num_list = {5, 2, 1, 7, 5};
+        int n = 3;
+        int[] answer = new int[num_list.length];
+        int cnt = 0;
+        for (int i = n; i<num_list.length; i++) {
+            answer[cnt++] = num_list[i];
+        }
+        for (int i = 0; i < n; i++) {
+            answer[cnt++] = num_list[i];
+        }
+        System.out.println(Arrays.toString(answer));
+    }
+}
