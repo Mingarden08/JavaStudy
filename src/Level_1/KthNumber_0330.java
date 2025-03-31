ea21544ea9b5b3a985b15914eaae8c856f03986a
Mingarden08 <jeongone.min@gmail.com>
2025-03-31 23:13:24 +0900

    K번째수

--- src/Level_1/KthNumber_0330.java
+++ src/Level_1/KthNumber_0330.java
@@ -1,36 +1,3 @@
-//package Level_1;
-//
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//import java.util.Comparator;
-//import java.util.List;
-//
-//public class KthNumber_0330 {
-//    public static void main(String[] args) {
-//        int[] array = {1, 5, 2, 6, 3, 7, 4};
-//        int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
-//        List<Integer> temp = new ArrayList<>();
-//        int[] answer = new int[commands.length];
-//        for (int i = 0; i<commands.length; i++) {
-//            for (int j = 0; j< array.length; j++) {
-//                if (j >= commands[i][0] - 1 && j < commands[i][1]) {
-//                    temp.add(array[j]);
-//                    System.out.println(i + " " + j + " " + array[j]);
-//                }
-//                if (j == commands[i][1] || (commands[i][1] - commands[i][0] == array.length - 1 && j == commands[i][1] - 1)) {
-//                    temp.sort(Comparator.naturalOrder());
-//                    answer[i] = temp.get(commands[i][2] - 1);
-//                    temp.clear();
-//                    System.out.println(temp);
-//                }
-//            }
-//        }
-//        System.out.println(Arrays.toString(answer));
-//        System.out.println(Arrays.deepToString(commands));
-//    }
-//}
-
-
 package Level_1;
 
 import java.util.Arrays;
