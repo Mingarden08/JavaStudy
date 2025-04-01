2dbdd98cd9ee88d85d6eb33d7e4d73fbe6554b9a
Mingarden08 <jeongone.min@gmail.com>
2025-04-01 23:08:45 +0900

    꼬리 문자열

---
+++ src/Level_0/TailString_0401.java
@@ -0,0 +1,18 @@
+package Level_0;
+
+public class TailString_0401 {
+    public static void main(String[] args) {
+        String[] str_list = {"abc", "def", "ghi"};
+        String ex = "ef";
+        String answer = "";
+        for (int i = 0; i<str_list.length; i++) {
+            if (str_list[i].contains(ex)) {
+                answer += "";
+            }
+            else {
+                answer += str_list[i];
+            }
+        }
+        System.out.println(answer);
+    }
+}
