78990afdfa245473ea52951a63ce5970ce418060
Mingarden08 <jeongone.min@gmail.com>
2025-04-03 23:05:47 +0900

    배열 만들기 1

---
+++ src/Level_0/CreatingAnArray1_0403.java
@@ -0,0 +1,19 @@
+package Level_0;
+
+import java.util.Arrays;
+
+public class CreatingAnArray1_0403 {
+    public static void main(String[] args) {
+        int n = 15;
+        int k = 5;
+        int cnt = 0;
+        int[] answer = new int[n/k];
+        for (int i = 1; i<=n; i++) {
+            if (i%k == 0) {
+                System.out.println(i);
+                answer[cnt++] = i;
+            }
+        }
+        System.out.println(Arrays.toString(answer));
+    }
+}
