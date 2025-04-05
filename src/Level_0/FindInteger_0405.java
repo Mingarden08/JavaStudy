bde15fb7dd9f9b8ab6b2a59f8c9e5c9667c1f135
Mingarden08 <jeongone.min@gmail.com>
2025-04-05 11:37:15 +0900

    정수 찾기

---
+++ src/Level_0/FindInteger_0405.java
@@ -0,0 +1,15 @@
+package Level_0;
+
+public class FindInteger_0405 {
+    public static void main(String[] args) {
+        int[] num_list = {15, 98, 23, 2, 15};
+        int n = 20;
+        int answer = 0;
+        for(int i = 0; i<num_list.length; i++) {
+            if (num_list[i] == n) {
+                answer = 1;
+            }
+        }
+        System.out.println(answer);
+    }
+}
