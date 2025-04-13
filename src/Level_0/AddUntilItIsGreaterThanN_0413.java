bb880dc18f134e2d29dd393ed65d05d1c4ced02c
Mingarden08 <jeongone.min@gmail.com>
2025-04-13 22:59:19 +0900

    n보다 커질 때까지 더하기

---
+++ src/Level_0/AddUntilItIsGreaterThanN_0413.java
@@ -0,0 +1,16 @@
+package Level_0;
+
+public class AddUntilItIsGreaterThanN_0413 {
+    public static void main(String[] args) {
+        int[] numbers = {34, 5, 71, 29, 100, 34};
+        int n = 123;
+        int answer = 0;
+        for(int i = 0; i<numbers.length; i++) {
+            answer = answer+numbers[i];
+            if (answer > n) {
+                break;
+            }
+        }
+        System.out.println(answer);
+    }
+}
