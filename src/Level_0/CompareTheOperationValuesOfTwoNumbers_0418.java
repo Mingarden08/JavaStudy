4b36356fa65ec13c77efe8a9539a34f67f6ff343
Mingarden08 <jeongone.min@gmail.com>
2025-04-20 22:25:39 +0900

    두 수의 연산값 비교하기

--- src/Level_0/CompareTheOperationValuesOfTwoNumbers_0418.java
+++ src/Level_0/CompareTheOperationValuesOfTwoNumbers_0418.java
@@ -4,6 +4,6 @@ public class CompareTheOperationValuesOfTwoNumbers_0418 {
     public static void main(String[] args) {
         int a = 2, b = 91;
 
-        System.out.println(a * b * 2 <= Integer.parseInt(Integer.toString(a)+Integer.toString(b)) ? Integer.parseInt(Integer.toString(a)+Integer.toString(b)) : a * b * 2);
+        System.out.println(a * b * 2 <= Integer.parseInt(a +Integer.toString(b)) ? Integer.parseInt(a +Integer.toString(b)) : a * b * 2);
     }
 }
