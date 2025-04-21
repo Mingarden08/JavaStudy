2292d038f9f32c48930b8370038b327b3c14f7e5
Mingarden08 <jeongone.min@gmail.com>
2025-04-21 21:57:29 +0900

    문자열 돌리기

---
+++ src/Level_0/TwistTheString_0421.java
@@ -0,0 +1,13 @@
+package Level_0;
+
+import java.util.Scanner;
+
+public class TwistTheString_0421 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        for (int i = 0; i<a.length(); i++) {
+            System.out.println(a.charAt(i));
+        }
+    }
+}
